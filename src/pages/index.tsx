2bb3197df887eac88a43e2d0e26a6e136303ece6
estaeldev <estael.dev@gmail.com>
2023-02-03 21:16:37 -0300

    Conteudo desenvolvido em aula

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,10 @@ import { FormEvent, useState } from 'react';
 import { useRouter } from 'next/router';
 import {GoOctoface} from 'react-icons/go'
 import styles from 'styles/home.module.scss'
-import {signIn, signOut, useSession} from 'next-auth/react'
 
 const HomePage: NextPage = () => {
     const router = useRouter()
     const [username, setUsername] = useState<string>('')
-    const {data} = useSession()
-
-    console.log(data);
-    
-
     const onSubmit = (event: FormEvent): void => {
         event.preventDefault()
         router.push(`/usuario/${username}`)
@@ -29,10 +23,6 @@ const HomePage: NextPage = () => {
                     onChange={event => setUsername(event.target.value)}
                 />
             </form>
-
-            <button onClick={() => signIn()} >Entrar</button>            
-            <button onClick={() => signOut()} >Sair</button>
-
         </div>
     )
 }
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,15 @@
+import { signIn } from "next-auth/react";
+import { GoOctoface } from "react-icons/go";
+import styles from './styles.module.scss'
+
+export default function LoginPage() {
+    return (
+        <div className={styles.container}>
+            <button onClick={() => signIn()}>
+                <GoOctoface />
+                Entrar
+            </button>
+        </div>
+    )
+
+}
